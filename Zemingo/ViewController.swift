b045f846342179f386887ce28aa09ca4053a9fbd
Assaf Tayouri <assaft753@gmail.com>
2020-12-07 13:19:16 +0200

    Initial Commit

---
+++ Zemingo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zemingo
+//
+//  Created by Assaf Tayouri on 07/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
